66c5e142c4f32a6d706fb6dcdf9bcb72614412ba
Teejay Dixon <tahjgdixon@gmail.com>
2022-08-13 03:49:56 -0700

    commit tree 59ec9e732f67d0832eb955a64f643b4fcace98a1

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat() {
+  return cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+  return cats.unshift("Bob");
+}
+
+destructivelyRemoveLastCat = () => {
+  return cats.pop(-1);
+}
+
+function destructivelyRemoveFirstCat() {
+  return cats.splice(0, 1);
+  
+}
+
+const newCats = [...cats]
+ 
+function appendCat() {
+  const newCats = [...cats, "Broom"];
+  return newCats;
+}
+
+function prependCat() {
+  const preCats = ["Arnold", ...cats];
+
+  return preCats;
+}
+
+
+function removeLastCat() {
+  const newCats = [...cats];
+  return cats.slice(0, newCats.length -1);
+}
+
+function removeFirstCat() {
+  const oldCats = [...cats];
+  return oldCats.splice(1,2);
+}
